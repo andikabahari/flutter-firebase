ab901885b854d26b98dc31982f0f293744dac7c4
Andika Bahari <kzlfxx@gmail.com>
2021-11-02 14:00:20 +0700

    user stream

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,7 +3,7 @@ import 'package:flutter_firebase/models/my_user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  MyUser? _firebaseUser(User? user) {
+  MyUser? _userFromFirebase(User? user) {
     if (user != null) {
       return MyUser(uid: user.uid);
     }
@@ -11,11 +11,15 @@ class AuthService {
     return null;
   }
 
+  Stream<MyUser?> get user {
+    return _auth.authStateChanges().map(_userFromFirebase);
+  }
+
   Future signInAnonymously() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return _firebaseUser(user);
+      return _userFromFirebase(user);
     } catch (e) {
       print(e.toString());
       return null;
