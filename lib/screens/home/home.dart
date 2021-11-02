b036b6e257b42aaf71bb77ac4b97989691ead5f7
Andika Bahari <kzlfxx@gmail.com>
2021-11-02 16:06:40 +0700

    sign out

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/services/auth.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home'),
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[50],
+      appBar: AppBar(
+        title: Text('Item List'),
+        backgroundColor: Colors.blueGrey[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          TextButton.icon(
+            onPressed: () async => await _auth.signOut(),
+            icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+            label: Text(
+              'Sign Out',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/models/my_user.dart';
 import 'package:flutter_firebase/screens/authenticate/authenticate.dart';
+import 'package:flutter_firebase/screens/home/home.dart';
 import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
@@ -9,7 +10,13 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<MyUser?>(context);
+
     print(user);
-    return Authenticate();
+
+    if (user == null) {
+      return Authenticate();
+    }
+
+    return Home();
   }
 }
