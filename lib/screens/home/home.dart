491896c26870dbc133f07c37e9ec4b739182cb8e
Andika Bahari <kzlfxx@gmail.com>
2021-11-06 18:15:43 +0700

    bottom sheet

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -11,6 +11,18 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+            child: Text('bottom sheet'),
+          );
+        },
+      );
+    }
+
     return StreamProvider<List<Item>>.value(
       initialData: [],
       value: DatabaseService().items,
@@ -33,6 +45,11 @@ class Home extends StatelessWidget {
                   color: Colors.white,
                 ),
               ),
+            ),
+            TextButton.icon(
+              onPressed: _showSettingsPanel,
+              icon: Icon(Icons.settings),
+              label: Text('Settings'),
             ),
           ],
         ),
