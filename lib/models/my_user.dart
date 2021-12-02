b05980f89d790ffa64138f23d4c63be4acfcd542
Andika Bahari <kzlfxx@gmail.com>
2021-12-02 14:42:49 +0700

    user data model

--- lib/models/my_user.dart
+++ lib/models/my_user.dart
@@ -3,3 +3,17 @@ class MyUser {
 
   MyUser({this.uid});
 }
+
+class UserData {
+  final String uid;
+  final String name;
+  final int chilies;
+  final int spicyLevel;
+
+  UserData({
+    required this.uid,
+    required this.name,
+    required this.chilies,
+    required this.spicyLevel,
+  });
+}
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_firebase/models/my_user.dart';
 import 'package:flutter_firebase/models/seblak.dart';
 
 class DatabaseService {
@@ -26,7 +27,21 @@ class DatabaseService {
     }).toList();
   }
 
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    Map<String, dynamic> data = snapshot.data()! as Map<String, dynamic>;
+    return UserData(
+      uid: uid!,
+      name: data['name'],
+      chilies: data['chilies'],
+      spicyLevel: data['spicyLevel'],
+    );
+  }
+
   Stream<List<Seblak>> get seblaks {
     return seblakCollection.snapshots().map(_seblakListFromSnapshot);
+  }
+
+  Stream<UserData> get userData {
+    return seblakCollection.doc(uid).snapshots().map(_userDataFromSnapshot);
   }
 }
